7b58fb84eb3eb9b02e7ab6e0edaca48b95acd5c8
aibayanyu <aibayanyu@qq.com>
2023-02-26 09:13:45 +0800

    feat: add site group

--- site/.vitepress/config/sidebar.ts
+++ site/.vitepress/config/sidebar.ts
@@ -1,10 +1,12 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const getSidebar = (): DefaultTheme.Sidebar => {
-  return [
-    {
-      text: 'Button 按钮',
-      link: '/components/button/'
-    }
-  ]
+  return {
+    '/components/': [
+      {
+        text: 'Button 按钮',
+        link: '/components/button/'
+      }
+    ]
+  }
 }
