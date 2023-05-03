febc297d5e59c5e138f51cf5097f723da20cf492
aibayanyu <aibayanyu@qq.com>
2023-05-03 19:43:56 +0800

    feat: add build

---
+++ vite.build.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
+export default defineConfig({
+  plugins: [
+    vueJsx(),
+    dts({
+      outputDir: ['es', 'lib'],
+      include: ['components/**/*.ts', 'components/**/*.tsx']
+    })
+  ],
+  build: {
+    rollupOptions: {
+      external: [
+        '@ant-design/colors',
+        /^@ant-design\/icons-vue/,
+        '@antd-tiny-vue/cssinjs',
+        '@ctrl/tinycolor',
+        '@v-c/utils',
+        '@vueuse/core',
+        'vue'
+      ],
+      output: [
+        {
+          format: 'es',
+          dir: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          preserveModulesRoot: 'components'
+        },
+        {
+          format: 'cjs',
+          dir: 'lib',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          preserveModulesRoot: 'components',
+          exports: 'named'
+        }
+      ]
+    },
+    lib: {
+      entry: 'components/index.ts',
+      formats: ['es', 'cjs']
+    }
+  }
+})
